814e0e36c4180e84031acc66e638475debae75c2
SURYA.P <115637636+rchfv@users.noreply.github.com>
2023-12-01 02:40:08 -0800

    s

---
+++ SQL File/studentmsdb.sql
@@ -0,0 +1,254 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 10, 2023 at 06:41 PM
+-- Server version: 10.3.15-MariaDB
+-- PHP Version: 8.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sturecdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbladmin`
+--
+
+CREATE TABLE `tbladmin` (
+  `ID` int(10) NOT NULL,
+  `AdminName` varchar(120) DEFAULT NULL,
+  `UserName` varchar(120) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `Password` varchar(200) DEFAULT NULL,
+  `AdminRegdate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbladmin`
+--
+
+INSERT INTO `tbladmin` (`ID`, `AdminName`, `UserName`, `MobileNumber`, `Email`, `Password`, `AdminRegdate`) VALUES
+(1, 'Admin', 'admin', 9750048622, 'admin@gmail.com', 'f925916e2754e5e03f75dd58a5733251', '2023-11-15 04:36:52');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblclass`
+--
+
+CREATE TABLE `tblclass` (
+  `ID` int(5) NOT NULL,
+  `ClassName` varchar(50) DEFAULT NULL,
+  `Batch` varchar(20) DEFAULT NULL,
+  `CreationDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblclass`
+--
+
+INSERT INTO `tblclass` (`ID`, `ClassName`, `Batch`, `CreationDate`) VALUES
+(1, 'Azure', '1', '2023-11-21 01:53:26'),
+(2, 'Azure', '2', '2023-11-21 01:55:41'),
+(3, 'Azure', '3', '2023-11-21 01:55:59'),
+(4, 'Azure', '4', '2023-11-21 01:56:10');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblnotice`
+--
+
+CREATE TABLE `tblnotice` (
+  `ID` int(5) NOT NULL,
+  `NoticeTitle` mediumtext DEFAULT NULL,
+  `ClassId` int(10) DEFAULT NULL,
+  `NoticeMsg` mediumtext DEFAULT NULL,
+  `CreationDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblnotice`
+--
+
+INSERT INTO `tblnotice` (`ID`, `NoticeTitle`, `ClassId`, `NoticeMsg`, `CreationDate`) VALUES
+(1, 'Project deployment	.', 19, 'project deployment on thursday', '2023-11-21 02:50:20');
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblpage`
+--
+
+CREATE TABLE `tblpage` (
+  `ID` int(10) NOT NULL,
+  `PageType` varchar(200) DEFAULT NULL,
+  `PageTitle` mediumtext DEFAULT NULL,
+  `PageDescription` mediumtext DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `UpdationDate` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblpage`
+--
+
+INSERT INTO `tblpage` (`ID`, `PageType`, `PageTitle`, `PageDescription`, `Email`, `MobileNumber`, `UpdationDate`) VALUES
+(1, 'aboutus', 'About Us', '<div style=\"text-align: start;\"><font color=\"#7b8898\" face=\"Mercury SSm A, Mercury SSm B, Georgia, Times, Times New Roman, Microsoft YaHei New, Microsoft Yahei, ????, ??, SimSun, STXihei, ????, serif\"><span style=\"font-size: 26px;\">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span></font><br></div>', NULL, NULL, NULL),
+(2, 'contactus', 'Contact Us', 'Magic Bus ,2nd floor, Upstreas to Hero Sprint, Madevakkam , Chennai,Tamil Nadu-600100', 'Magicbus@gmail.com', 2262434848, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblpublicnotice`
+--
+
+CREATE TABLE `tblpublicnotice` (
+  `ID` int(5) NOT NULL,
+  `NoticeTitle` varchar(200) DEFAULT NULL,
+  `NoticeMessage` mediumtext DEFAULT NULL,
+  `CreationDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblpublicnotice`
+--
+
+INSERT INTO `tblpublicnotice` (`ID`, `NoticeTitle`, `NoticeMessage`, `CreationDate`) VALUES
+(3,'Project deployment','2023-11-23','2023-11-20 09:23:16');
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblstudent`
+--
+
+CREATE TABLE `tblstudent` (
+  `ID` int(10) NOT NULL,
+  `StudentName` varchar(200) DEFAULT NULL,
+  `StudentEmail` varchar(200) DEFAULT NULL,
+  `StudentClass` varchar(100) DEFAULT NULL,
+  `Gender` varchar(50) DEFAULT NULL,
+  `DOB` date DEFAULT NULL,
+  `StuID` varchar(200) DEFAULT NULL,
+  `FatherName` mediumtext DEFAULT NULL,
+  `MotherName` mediumtext DEFAULT NULL,
+  `ContactNumber` bigint(10) DEFAULT NULL,
+  `AltenateNumber` bigint(10) DEFAULT NULL,
+  `Address` mediumtext DEFAULT NULL,
+  `UserName` varchar(200) DEFAULT NULL,
+  `Password` varchar(200) DEFAULT NULL,
+  `Image` varchar(200) DEFAULT NULL,
+  `DateofAdmission` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tblstudent`
+--
+
+INSERT INTO `tblstudent` (`ID`, `StudentName`, `StudentEmail`, `StudentClass`, `Gender`, `DOB`, `StuID`, `FatherName`, `MotherName`, `ContactNumber`, `AltenateNumber`, `Address`, `UserName`, `Password`, `Image`, `DateofAdmission`) VALUES
+(1, 'karuna', 'karuna@gmail.com', '4', 'Male', '2001-12-03', '2105', 'parasuraman', 'punitha', 6381016170, 4646546565, 'no3,perumal koil street chettipedu', 'karuna', '202cb962ac59075b964b07152d234b70', 'ebcd036a0db50db993ae98ce380f64191642082944.png', '2023-10-13 14:09:04');
+
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblclass`
+--
+ALTER TABLE `tblclass`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblnotice`
+--
+ALTER TABLE `tblnotice`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblpage`
+--
+ALTER TABLE `tblpage`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblpublicnotice`
+--
+ALTER TABLE `tblpublicnotice`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblstudent`
+--
+ALTER TABLE `tblstudent`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tblclass`
+--
+ALTER TABLE `tblclass`
+  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `tblnotice`
+--
+ALTER TABLE `tblnotice`
+  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tblpage`
+--
+ALTER TABLE `tblpage`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tblpublicnotice`
+--
+ALTER TABLE `tblpublicnotice`
+  MODIFY `ID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tblstudent`
+--
+ALTER TABLE `tblstudent`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ includes/header.php
@@ -0,0 +1,73 @@
+<!--header-->
+    <div class="header" id="home">
+      <nav class="navbar navbar-default">
+        <div class="container">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"> </span>
+            <span class="icon-bar"> </span>
+            <span class="icon-bar"> </span>
+          </button>
+
+          
+         
+          <h1><a class="navbar-brand" href="index.php">MBSMS</a></h1>
+          
+          </div>
+          <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav navbar-right margin-top cl-effect-2">
+                <li><a href="index.php"><span data-hover="Home">Home</span></a></li>
+                <li><a href="about.php"><span data-hover="About">About</span></a></li>
+                
+                <li><a href="contact.php"><span data-hover="Contact">Contact</span></a></li>
+                <li><a href="admin/login.php"><span data-hover="Contact">Admin</span></a></li>
+                <li><a href="user/login.php"><span data-hover="Shortcodes">Student</span></a></li>
+                
+              </ul>
+              <div class="clearfix"> </div>
+            </div> 
+            
+            
+            
+            
+            <!-- /.navbar-collapse -->
+        <!-- /.container-fluid -->
+
+        <!--  -->
+
+
+        
+      </nav>
+      
+<!--/script-->
+       <div class="clearfix"> </div>
+</div>
+<!-- Top Navigation -->
+<a href="#" class="float">
+<img src="./images/chatbot1.png" width="60" height="60" data-hover="Chatbot"/>
+</a>
+<style>
+  
+.float{
+	position:fixed;
+	width:60px;
+  float:left;
+	height:60px;
+	bottom:40px;
+	left:20px;
+	background-color:#0C9;
+	color:#FFF;
+	border-radius:50px;
+	text-align:center;
+	box-shadow: 2px 2px 3px #999;
+  z-index: 999;
+}
+
+.my-float{
+	margin-top:22px;
+}
+</style>
+<!--header-->
